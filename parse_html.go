f18edbb720ae87098ad8360baeb1a4085de29510
Michael Greaves <michael.greaves@epidemicsound.com>
2025-09-26 15:53:42 +0200

    Update progress

--- parse_html.go
+++ parse_html.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type PageData struct {
+	URL            string
+	H1             string
+	FirstParagraph string
+	OutgoingLinks  []string
+	ImageURLs      []string
+}
 
 func getH1FromHTML(html string) (string, error) {
 	htmlReader := strings.NewReader(html)
@@ -30,10 +39,77 @@ func getFirstParagraphFromHTML(html string) (string, error) {
 	main := doc.Find("main")
 	var firstPar string
 	if main.Length() > 0 {
-		firstPar = doc.Find("p").First().Text()
-	} else {
 		firstPar = main.Find("p").First().Text()
+	} else {
+		firstPar = doc.Find("p").First().Text()
 	}
 
 	return strings.TrimSpace(firstPar), nil
+}
+
+func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	htmlReader := strings.NewReader(htmlBody)
+
+	doc, err := goquery.NewDocumentFromReader(htmlReader)
+	if err != nil {
+		return nil, fmt.Errorf("Unable parse html: %v", err)
+	}
+
+	hrefs := []string{}
+	doc.Find("a[href]").Each(func(_ int, s *goquery.Selection) {
+		href, ok := s.Attr("href")
+		if !ok || strings.TrimSpace(href) == "" {
+			return
+		}
+
+		parsed, err := url.Parse(href)
+		if err != nil {
+			return
+		}
+		hrefURL := baseURL.ResolveReference(parsed)
+		hrefs = append(hrefs, hrefURL.String())
+	})
+	return hrefs, nil
+}
+
+func getImagesFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	htmlReader := strings.NewReader(htmlBody)
+
+	doc, err := goquery.NewDocumentFromReader(htmlReader)
+	if err != nil {
+		return nil, fmt.Errorf("Unable parse html: %v", err)
+	}
+
+	imgs := []string{}
+	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+		src, ok := s.Attr("src")
+		if !ok || strings.TrimSpace(src) == "" {
+			return
+		}
+
+		parsed, err := url.Parse(src)
+		if err != nil {
+			return
+		}
+		imgURL := baseURL.ResolveReference(parsed)
+		imgs = append(imgs, imgURL.String())
+	})
+	return imgs, nil
+
+}
+
+func extractPageData(html, pageURL string) PageData {
+	header, _ := getH1FromHTML(html)
+	firstParagraph, _ := getFirstParagraphFromHTML(html)
+	parsedURL, _ := url.Parse(pageURL)
+	outgoingLinks, _ := getURLsFromHTML(html, parsedURL)
+	imageURLs, _ := getImagesFromHTML(html, parsedURL)
+
+	return PageData{
+		URL: pageURL,
+		H1: header,
+		FirstParagraph: firstParagraph,
+		OutgoingLinks: outgoingLinks,
+		ImageURLs: imageURLs,
+	}
 }
--- parse_html_test.go
+++ parse_html_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/url"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -70,5 +72,147 @@ func TestGetFirstParagraphFromHTMLMainPriority(t *testing.T) {
 				t.Errorf("Test %v - '%s' FAIL: expected '%s' but got '%s'", i, tc.name, tc.expected, actual)
 			}
 		})
+	}
+}
+
+func TestGetURLsFromHTML(t *testing.T) {
+	testCases := []struct{
+		name string
+		inputBody string
+		inputURL string
+		expected []string
+	}{
+		{
+			name: "Absolute reference",
+			inputBody: `<html><body><a href="https://blog.boot.dev"><span>Boot.dev</span></a></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{"https://blog.boot.dev"},
+		},
+		{
+			name: "nested reference",
+			inputBody: `<html><body><a href="/posts/myfancynewpost"><span>Boot.dev</span></a></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{"https://blog.boot.dev/posts/myfancynewpost"},
+		},
+		{
+			name: "external reference",
+			inputBody: `<html><body><a href="https://www.google.com"><span>Boot.dev</span></a></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{"https://www.google.com"},
+		},
+		{
+			name: "multi reference",
+			inputBody: `<html><body><a href="/posts/thisisonepost"><a href="/posts/thisisanotherpost"><span>Boot.dev</span></a></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{"https://blog.boot.dev/posts/thisisonepost", "https://blog.boot.dev/posts/thisisanotherpost"},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			baseURL, err := url.Parse(tc.inputURL)
+			if err != nil {
+				t.Errorf("couldn't parse input URL: %v", err)
+				return
+			}
+			
+			actual, err := getURLsFromHTML(tc.inputBody, baseURL)
+			if err != nil {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Test %v - '%s' FAIL: expected '%s' but got '%s'", i, tc.name, tc.expected, actual)
+			}
+		})
+
+	}
+}
+
+func TestGetImagessFromHTML(t *testing.T) {
+	testCases := []struct{
+		name string
+		inputBody string
+		inputURL string
+		expected []string
+	}{
+		{
+			name: "Relative reference",
+			inputBody: `<html><body><img src="/logo.png" alt="Logo"></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{"https://blog.boot.dev/logo.png"},
+		},
+		{
+			name: "Missing reference",
+			inputBody: `<html><body></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{},
+		},
+		{
+			name: "Multiple reference",
+			inputBody: `<html><body><img src="/logo.png" alt="Logo"><img src="/logo2.png" alt="Logo2"></body></html>`,
+			inputURL: "https://blog.boot.dev",
+			expected: []string{"https://blog.boot.dev/logo.png", "https://blog.boot.dev/logo2.png"},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			baseURL, err := url.Parse(tc.inputURL)
+			if err != nil {
+				t.Errorf("couldn't parse input URL: %v", err)
+				return
+			}
+			
+			actual, err := getImagesFromHTML(tc.inputBody, baseURL)
+			if err != nil {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Test %v - '%s' FAIL: expected '%s' but got '%s'", i, tc.name, tc.expected, actual)
+			}
+		})
+
+	}
+}
+
+func TestExtractPageData(t *testing.T) {
+	testCases := []struct{
+		name string
+		inputBody string
+		inputURL string
+		expected PageData
+	}{
+		{
+			name: "Basic",
+			inputBody: `<html><body>
+        <h1>Test Title</h1>
+        <p>This is the first paragraph.</p>
+        <a href="/link1">Link 1</a>
+        <img src="/image1.jpg" alt="Image 1">
+    </body></html>`, 
+			inputURL: "https://blog.boot.dev",
+			expected: PageData{
+				URL: "https://blog.boot.dev",
+				H1: "Test Title",
+				FirstParagraph: "This is the first paragraph.",
+				OutgoingLinks: []string{"https://blog.boot.dev/link1"},
+				ImageURLs: []string{"https://blog.boot.dev/image1.jpg"},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual:= extractPageData(tc.inputBody, tc.inputURL)
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Test %v - '%s' FAIL: expected '%+v' but got '%+v'", i, tc.name, tc.expected, actual)
+			}
+		})
+
 	}
 }
